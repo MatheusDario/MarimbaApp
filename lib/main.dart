b9ba374b4d5a8627f58917785d2529798919ce01
Matheus Dario <dario.matheus97@gmail.com>
2023-08-22 20:32:05 -0300

    criando func som e add btns

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,13 @@ void main() {
 }
 
 class MarimbaApp extends StatelessWidget {
+
+  void tocarSom(int numNotas) {
+    final player = AudioPlayer();
+    player.play(AssetSource('nota$numNotas.wav'));
+  }
+
+
   MarimbaApp({super.key});
 
   final player = AudioPlayer();
@@ -15,13 +22,122 @@ class MarimbaApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () async {
-                final player = AudioPlayer();
-                player.play(AssetSource('nota2.wav'));
-              },
-              child: Text('Clique aqui!'),
+          child: Container(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.green, // Background color
+                      ),
+                      onPressed: () {
+                       tocarSom(1);
+                      },
+                      child: const SizedBox(
+                        width: 100.0,
+                        height: 50.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blue, // Background color
+                      ),
+                      onPressed: () {
+                        tocarSom(2);
+                      },
+                      child: Container(
+                        width: 100.0,
+                        height: 50.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.yellow, // Background color
+                      ),
+                      onPressed: () {
+                        tocarSom(3);
+                      },
+                      child: const SizedBox(
+                        width: 100.0,
+                        height: 50.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.red, // Background color
+                      ),
+                      onPressed: () {
+                       tocarSom(4);
+                      },
+                      child: const SizedBox(
+                        width: 100.0,
+                        height: 50.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.grey, // Background color
+                      ),
+                      onPressed: () {
+                        tocarSom(5);
+                      },
+                      child: const SizedBox(
+                        width: 100.0,
+                        height: 50.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.purple, // Background color
+                      ),
+                      onPressed: () {
+                        tocarSom(6);
+                      },
+                      child: const SizedBox(
+                        width: 100.0,
+                        height: 50.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.brown, // Background color
+                      ),
+                      onPressed: () {
+                       tocarSom(7);
+                      },
+                      child: const SizedBox(
+                        width: 100.0,
+                        height: 50.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
